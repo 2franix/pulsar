37dea520b12814ee2402ee969280b403e28025fa
Cyrille Defranoux <cyrille.github@youplaboum.fr>
2016-12-16 22:59:38 +0100

    Added mainloop and mainloop API.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <memory>
 
 #include <pulse/thread-mainloop.h>
 
 using namespace std;
+
+class Monitor
+{
+	private:
+		/** Alias for std::unique_ptr dedicated to PA objects that require a
+		 * specific function for disposal. */
+		template <class T> using Ptr = std::unique_ptr<T, std::function<void (T*)>>;
+
+	public:
+		Monitor()
+			: mainloop(pa_threaded_mainloop_new(), pa_threaded_mainloop_free), mainloopApi(nullptr)
+			{
+				this->mainloopApi = pa_threaded_mainloop_get_api(this->mainloop.get());
+			}
+
+	private:
+		Ptr<pa_threaded_mainloop> mainloop;
+		pa_mainloop_api *mainloopApi;
+};
 
 int main(int argumentCount, char **arguments)
 {
@@ -10,6 +30,6 @@ int main(int argumentCount, char **arguments)
 	{
 		cout << arguments[i] << endl;
 	}
-	pa_threaded_mainloop_new();
+	Monitor monitor;
 	return 0;
 };
