48853c8563146c3fd3cd30fabc5505b7798cdd66
Cyrille Defranoux <cyrille.github@youplaboum.fr>
2020-04-20 23:25:57 +0200

    Added operator to combine sinks

--- src/main.cpp
+++ src/main.cpp
@@ -27,9 +27,10 @@ int main(int argumentCount, char **arguments)
 	desc.add_options()
 		("server", po::value<std::string>()->default_value(""), "Server to connect to.")
 		("sinks,s", po::value<std::vector<std::string>>()->multitoken(), "Sinks to connect to.")
+		("operator,o", po::value<std::string>()->multitoken()->default_value("or"), "Operator to apply on the different sinks for probing to succeed.")
 		("timeout,t", po::value<double>()->default_value(0.5), "Probing duration in seconds. It is the maximum amount of time before the program will return a non-zero value if no sample could be probed.")
 		("verbosity,v", po::value<string>()->default_value("error"), "Verbose mode. Value must be in [info, warning, error].")
-		("version", "Print version of this program.")
+		("version", po::bool_switch()->default_value(false), "Print version of this program.")
 		("help,h", "Print this message.");
 
 	// Parse command line.
@@ -55,9 +56,17 @@ int main(int argumentCount, char **arguments)
 	}
 	Application::get().setLogLevel(logLevel);
 
-	if (vm.count("version") > 0)
+	if (vm["version"].as<bool>())
 	{
 		cout << Application::get().getVersion() << endl;
+		return 0;
+	}
+
+	const string &oper = vm["operator"].as<string>();
+	if (oper != "and" && oper != "or")
+	{
+		Application::get().printError("Invalid operator.");
+		return 2;
 	}
 
 	if (vm.count("help") > 0)
@@ -90,9 +99,23 @@ int main(int argumentCount, char **arguments)
 	double duration = vm["timeout"].as<double>();
 	double startTime = getTime();
 
-	while(getTime() - startTime < duration &&
-		std::none_of(monitors.begin(), monitors.end(), [](const shared_ptr<Monitor> &monitor) {return monitor->hasSamples();}))
+	std::function<bool(const list<shared_ptr<Monitor>>&)> predicate = [&oper](const list<shared_ptr<Monitor>>& monitors)
 	{
+		std::function<bool(const shared_ptr<Monitor>&)> monitorPredicate = [](const shared_ptr<Monitor> &monitor) {return monitor->hasSamples();};
+		if (oper == "or")
+		{
+			return any_of(monitors.begin(), monitors.end(), monitorPredicate);
+		}
+		else
+		{
+			return all_of(monitors.begin(), monitors.end(), monitorPredicate); 
+		}
+	};
+
+	bool sampleFound = false;
+	while(getTime() - startTime < duration && !sampleFound)
+	{
+		sampleFound = predicate(monitors);
 		std::this_thread::sleep_for(std::chrono::milliseconds(50));
 	}
 
@@ -105,5 +128,5 @@ int main(int argumentCount, char **arguments)
 
 	pa_threaded_mainloop_stop(mainloop.get());
 	
-	return std::any_of(monitors.begin(), monitors.end(), [](const shared_ptr<Monitor> &monitor) {return monitor->hasSamples();}) ? 0 : 1;
+	return sampleFound ? 0 : 1;
 };
